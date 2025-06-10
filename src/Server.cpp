10d0118d286069263d6478db57428928261ef1e5
pablooglez <pablogon@student.42malaga.com>
2025-06-10 19:54:07 +0200

    Implemented HandleNewConnection()

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: pablogon <pablogon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 17:05:24 by pablogon          #+#    #+#             */
-/*   Updated: 2025/06/10 18:02:57 by pablogon         ###   ########.fr       */
+/*   Updated: 2025/06/10 19:53:01 by pablogon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,7 @@ bool	Server::setupSocket()
 	server_pollfd.events = POLLIN;
 	server_pollfd.revents = 0;
 
-	this->_poll_fds.push_back(server_pollfd);	// Add server socket to poll arraya
+	this->_poll_fds.push_back(server_pollfd);	// Add server socket to poll array
 
 	
 	std::cout << "Server socket created succesfully" << std::endl;
@@ -125,18 +125,15 @@ void	Server::stop()
 
 void	Server::cleanup()
 {
-	// Cerrar todos los sockets de clientes
-	for (size_t i = 0; i < this->_client_fds.size(); i++)
+	for (size_t i = 0; i < this->_client_fds.size(); i++) // Close all client sockets
 	{
 		close(this->_client_fds[i]);
 	}
 	this->_client_fds.clear();
-	
-	// Limpiar array de poll
-	this->_poll_fds.clear();
-	
-	// Cerrar socket del servidor
-	if (this->_server_fd != -1)
+
+	this->_poll_fds.clear(); // Clean poll array
+
+	if (this->_server_fd != -1) // Close server socket
 	{
 		close(this->_server_fd);
 		this->_server_fd = -1;
@@ -188,7 +185,7 @@ void	Server::runServerLoop()
 				// Handle client disconnection
 				if (this->_poll_fds[i].fd != this->_server_fd)
 				{
-					std::cout << "Error: Client disconnected (error/hangup) (fd: " << this->_poll_fds[i].fd << ")" << std::endl;
+					std::cout << "Error: Client disconnected (error/hangup)" << std::endl;
 					removeClient(this->_poll_fds[i].fd);
 					i--;	// Adjust index because we removed an element
 				}
@@ -197,3 +194,46 @@ void	Server::runServerLoop()
 	}
 	std::cout << "    SERVER STOPPED    " << std::endl;
 }
+
+void	Server::handleNewConnection()
+{
+	std::cout << "...Handling new connection..." << std::endl;
+
+	struct sockaddr_in client_addr;	// Structure for handling customer information
+	socklen_t client_len = sizeof(client_addr);
+
+	//Accept() accept pending connection
+	int	client_fd = accept(this->_server_fd, (struct sockaddr*) &client_addr, &client_len);
+	
+	if (client_fd == -1)
+	{
+		std::cerr << "Error: accept() failed" << std::endl;
+		return;
+	}
+
+	if (fcntl(client_fd, F_SETFL, O_NONBLOCK) == -1) // Socket Not-blocking
+	{
+		std::cerr << "Error: fcntl() failed for client socket" << std::endl;
+		close(client_fd);
+		return;
+	}
+
+	// Add the client to the poll array
+	struct pollfd client_pollfd;
+	client_pollfd.fd = client_fd;
+	client_pollfd.events = POLLIN;
+	client_pollfd.events = 0;
+
+	this->_poll_fds.push_back(client_pollfd);
+	this->_client_fds.push_back(client_fd);
+
+	char	client_ip[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+
+	std::cout << "New client connected:" << std::endl;
+	std::cout << "  - FD: " << client_fd << std::endl;
+	std::cout << "  - IP: " << client_ip << std::endl;
+	std::cout << "  - Port: " << ntohs(client_addr.sin_port) << std::endl;
+	std::cout << "  - Total clients: " << this->_client_fds.size() << std::endl;
+}
+
